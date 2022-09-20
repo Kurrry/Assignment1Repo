86a75330a9c07a36e19d65f029817de1a5adb3d7
Mooncaked <99844142+Mooncaked@users.noreply.github.com>
2022-09-19 22:35:59 -0600

    Update AppDriver.java
    Changed to account for recursion

--- AppDriver.java
+++ AppDriver.java
@@ -7,30 +7,9 @@ public class AppDriver {
 		LibraryUser lu1 = new LibraryUser();
 		lu1.loadDoc();
 		lu1.loadMenu();
-		int userInput = userInputer.nextInt();
-		while (userInput != 5) {
-			switch (userInput) {
-				case 1:
-					lu1.checkoutBook();
-					break;
-				case 2:
-					lu1.findBook();
-					break;
-				case 3:
-					lu1.bookByType();
-					break;
-				case 4:
-					lu1.randomBookList();
-					break;
-			}
-			lu1.loadMenu();
-			userInput = userInputer.nextInt();
-		}
-		if (userInput == 5) {
-			lu1.saveBookList();
-			userInputer.close();
-			System.out.println("Have a great rest of your day!");
-			System.exit(0);
+		userInputer.close();
+		System.out.println("Have a great rest of your day!");
+		System.exit(0);
 		}
 	}
 
